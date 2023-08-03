26b5323f9c5a7e3811fd93c82a1f8353c9a81f77
Coda Bool <codabool@pm.me>
2023-08-03 15:22:38 -0400

    cleanup

--- HideServersHideChannels.plugin.js
+++ HideServersHideChannels.plugin.js
@@ -4,44 +4,42 @@
  * @description Hotkey only way of hiding channels or hiding servers
  * @author Codabool
  * @website https://codabool.com
- * @source https://github.com/CodaBool/hide_server_hide_channels
- * @updateUrl https://raw.githubusercontent.com/Pieloaf/BetterDiscordPlugins/main/HideSidebar.plugin.js
+ * @source https://github.com/CodaBool/hide_server_hide_channels/blob/main/HideServersHideChannels.plugin.js
+ * @updateUrl https://raw.githubusercontent.com/CodaBool/hide_server_hide_channels/main/HideServersHideChannels.plugin.js
  */
-module.exports = (_ => {
 
-  function createSelector(className) {
-    return '.' + className.replace(/ /g, '.')
+const sidebarSelector = createSelector(
+  document.querySelector('[class^="sidebar"]').className
+)
+const guildBarSelector = createSelector(
+  document.querySelector('nav[class*="guilds-"').className
+)
+
+function createSelector(className) {
+  return '.' + className.replace(/ /g, '.')
+}
+
+function toggleView(el) {
+  if (el.style.display === "" || el.style.display === "flex") {
+    el.style.display = "none"
+  } else {
+    el.style.display = "flex"
   }
+}
 
-  const sidebarSelector = createSelector(
-    document.querySelector('[class^="sidebar"]').className
-  );
-  const guildBarSelector = createSelector(
-    document.querySelector('nav[class*="guilds-"').className
-  );
-
-  const toggleView = (selectedEl) => {
-    if (
-      selectedEl.style.display === "" ||
-      selectedEl.style.display === "flex"
-    ) {
-      selectedEl.style.display = "none";
-    } else {
-      selectedEl.style.display = "flex";
-    }
+function handler(event) {
+  if (event.altKey && event.key.toLowerCase() === "s") {
+    toggleView(document.querySelector(guildBarSelector))
+  } else if (event.altKey && event.key.toLowerCase() === "c") {
+    toggleView(document.querySelector(sidebarSelector))
   }
+}
 
-  document.onkeydown = function(evt) {
-    let alt = evt.altKey;
-    let cKey = evt.key.toLowerCase() === "c";
-    let sKey = evt.key.toLowerCase() === "s";
-    if (alt && cKey)
-      toggleView(document.querySelector(sidebarSelector));
-    if (alt && sKey)
-      toggleView(document.querySelector(guildBarSelector));
-  };
-  return class {
-    start() {}
-    stop() {}
+module.exports = () => ({
+  start() {
+    document.addEventListener("keydown", handler)
+  },
+  stop() {
+    document.removeEventListener("keydown", handler)
   }
-})();
+})
